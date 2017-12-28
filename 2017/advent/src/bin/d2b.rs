13991b487c845e9efd926679c99a0a057e66023e
Michael Mokrysz <hi@46b.it>
2017-12-28 15:03:28 +0000

    Day 3 Part A solution
    I feel like I overcomplicated this one. I could try and practice using
    OEIS if I want to solve these ones manually.

--- 2017/advent/src/bin/d2b.rs
+++ 2017/advent/src/bin/d2b.rs
@@ -3,19 +3,25 @@ use std::io::{stdin, Read};
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
-    let output = d2a(&input);
+    let output = d2b(&input);
     println!("{:?}", output);
 }
 
-fn d2a(input: &str) -> u32 {
+fn d2b(input: &str) -> u32 {
     matrix(input)
         .into_iter()
-        .map(|mut row| {
-            row.sort_unstable();
-            match (row.first(), row.last()) {
-                (Some(least), Some(most)) => most - least,
-                _ => 0,
+        .map(|row| {
+            for (i, n) in row.iter().enumerate() {
+                for (j, m) in row.iter().enumerate() {
+                    if i == j {
+                        continue;
+                    }
+                    if (n / m) * m == *n {
+                        return n / m;
+                    }
+                }
             }
+            unreachable!();
         })
         .sum()
 }
@@ -37,11 +43,6 @@ mod tests {
 
     #[test]
     fn provided_testcase() {
-        assert_eq!(d2a("5 1 9 5\n 7 5 3\n 2 4 6 8"), 18);
-    }
-
-    #[test]
-    fn multidigit_testcase() {
-        assert_eq!(d2a("59 1 9 5\n 7 5 3\n 2 4 6 8"), 68);
+        assert_eq!(d2b("5 9 2 8\n 9 4 7 3 \n3 8 6 5"), 9);
     }
 }
